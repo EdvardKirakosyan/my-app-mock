6a1c3e170a9578726b60d1a4cb4b948c9053d5cd
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-15 14:24:03 +0400

    d

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-app-mock' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app-mock');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app-mock');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, my-app-mock'
+    );
   });
 });
