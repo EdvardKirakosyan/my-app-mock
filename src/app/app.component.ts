0aa22c1657d20327b0e6a257c55cf99f149a3e58
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-15 14:44:36 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServerComponent } from './server/server.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ServerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
