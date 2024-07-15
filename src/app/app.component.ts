24ae92177f3a5a449d619532171804fa869e888a
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-15 15:23:29 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServersComponent],
+  imports: [
+    RouterOutlet,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   templateUrl: './app.component.html',
   // styleUrl: './app.component.scss',
   styles: [
