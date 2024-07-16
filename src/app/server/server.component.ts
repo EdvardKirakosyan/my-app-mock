09e75d09122bf2336a6ef61b04c5ac667966c5c4
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-16 11:53:43 +0400

    c

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
+  imports: [CommonModule],
   templateUrl: './server.component.html',
   standalone: true,
 })
@@ -9,7 +11,15 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'ofline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
